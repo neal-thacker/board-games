a1856254965ce40c02f290865986b20428eddb53
Neal Thacker <nealsthacker@gmail.com>
2025-08-22 20:47:41 -0400

    Update package dependencies and configure Webpack for browser compatibility

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -28,11 +29,28 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    fallback: {
+      "process": require.resolve("process/browser"),
+      "buffer": require.resolve("buffer"),
+      "util": require.resolve("util"),
+      "stream": require.resolve("stream-browserify"),
+      "crypto": false,
+      "fs": false,
+      "path": false,
+      "os": false
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
   ],
   devServer: {
     static: {
