a95f93f69e7a129e8a839ddec2343e3298f9be5b
Neal Thacker <nealsthacker@gmail.com>
2025-08-24 20:13:50 -0400

    feat: add publicPath configuration to webpack output settings

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: isProduction ? '[name].[contenthash].js' : 'main.js',
+    publicPath: '/',
     clean: true,
   },
   experiments: {
