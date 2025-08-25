4db472f68fb57d74bfef551ca00655c62616439d
Neal Thacker <nealsthacker@gmail.com>
2025-08-24 20:21:57 -0400

    feat: add server-info endpoint to return server IP and hostname

--- backend/app/routes/api.php
+++ backend/app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\TagController;
@@ -11,3 +12,10 @@ Route::get('games-age-stats', [GameController::class, 'ageStats']);
 Route::apiResource('tags', TagController::class);
 Route::post('games/{game}/tags/{tag}', [GameTagController::class, 'attach']);
 Route::delete('games/{game}/tags/{tag}', [GameTagController::class, 'detach']);
+
+Route::get('/server-info', function (Request $request) {
+    return response()->json([
+        'ip' => $request->server('SERVER_ADDR') ?? gethostbyname(gethostname()),
+        'host' => gethostname(),
+    ]);
+});
